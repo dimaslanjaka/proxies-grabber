f8c57ab812dd0cb284ac593dffd62cc534d44547
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-29 04:02:52 +0700

    update

---
+++ gulpfile.ts
@@ -0,0 +1,55 @@
+import gulp from 'gulp';
+import proxyGrabber from './src/core';
+import { join } from 'upath';
+import { existsSync, mkdirSync } from 'fs';
+import dom from 'gulp-jsdom';
+import generateIndex from './src-docs';
+
+const grabber = new proxyGrabber();
+gulp.task('method3', (done) => {
+  grabber
+    .method3(true)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(console.log)
+    .finally(done);
+});
+gulp.task('method2', (done) => {
+  grabber
+    .method2(true)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(console.log)
+    .finally(done);
+});
+gulp.task('method1', (done) => {
+  grabber
+    .method1(true)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(console.log)
+    .finally(done);
+});
+
+gulp.task('docs', async () => {
+  const dest = join(__dirname, 'docs');
+  if (!existsSync(dest)) mkdirSync(dest);
+  await generateIndex();
+  // @todo [docs] transform readme.md to index.html
+  gulp.src(['readme.md', 'gulpfile*.{js,ts}'], { cwd: __dirname }).pipe(gulp.dest(dest));
+  // @todo [docs] modify external links
+  gulp
+    .src('**/*.html', { cwd: dest })
+    .pipe(
+      dom((document: Document) => {
+        Array.from(document.querySelectorAll('a')).forEach((el) => {
+          el.target = '_blank';
+          el.rel = 'nofollow noopener noreferer';
+        });
+      }),
+    )
+    .pipe(gulp.dest(dest));
+});
